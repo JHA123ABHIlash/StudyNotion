98e4d475b7f4ef3745fd3144dca7c638f746394d
Abhilash Jha <abhilashjha121@gmail.com>
2026-08-20 00:05:20 +0530

    Upload StudyNotion Project

--- backend/controllers/ResetPassword.js
+++ backend/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `https://study-notion-dusky.vercel.app/update-password/${token}`;
+		const url = `${process.env.FRONTEND_URL}/update-password/${token}`;
 
 		await mailSender(
 			email,
--- backend/mail/templates/emailVerificationTemplate.js
+++ backend/mail/templates/emailVerificationTemplate.js
@@ -66,8 +66,9 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-					src="https://private-user-images.githubusercontent.com/89846475/443247273-acbc6cde-9057-44e8-87f0-bd8163adaf0b.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDcxNDU2NTUsIm5iZiI6MTc0NzE0NTM1NSwicGF0aCI6Ii84OTg0NjQ3NS80NDMyNDcyNzMtYWNiYzZjZGUtOTA1Ny00NGU4LTg3ZjAtYmQ4MTYzYWRhZjBiLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA1MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNTEzVDE0MDkxNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTJkYmQ1ZTViMmMxYmUyNjhmZDQ3MDE4YjhjMWFkNjI0OGI5N2VmZmUwMDIwYWE0NDczNDY4ODVkYjQ5ZjAwMWYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.L-YCwmwUvD17147-ZXL08xrKbPkkRLeAWiKb_xOqPkc" alt="StudyNotion Logo"></a>
+			<a href="${process.env.FRONTEND_URL}"><img class="logo"
+        src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
